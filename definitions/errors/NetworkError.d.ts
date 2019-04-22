c201c73241208aaacec8894ed7087f167a398fd6
jiseung <91debug@room36.co>
2019-04-22 16:10:12 +0900

    쓰레기파일 삭제

--- definitions/errors/NetworkError.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * request error
- */
-export declare class RequestError extends Error {
-    name: string;
-    constructor(url: string);
-}
-//# sourceMappingURL=NetworkError.d.ts.map
--- definitions/errors/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './NetworkError';
-export * from './ResponseError';
-//# sourceMappingURL=index.d.ts.map
